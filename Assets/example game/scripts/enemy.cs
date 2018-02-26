eca4cbc83fa88bfeccf5fb542d1eedd9f15ad010
Ribbium <36156984+Ribbium@users.noreply.github.com>
2018-02-26 12:51:38 +0000

    added betetr collision

---
+++ Assets/example game/scripts/enemy.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class enemy : MonoBehaviour {
+
+    // Use this for initialization
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+
+  void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("the jews are to blame");
+
+        player mario = collision.collider.GetComponent<player>();
+        mario.damage(2);
+    }
+    
+}
--- Assets/example game/scripts/player.cs
+++ Assets/example game/scripts/player.cs
@@ -4,21 +4,32 @@ using System.Collections;
 public class player : MonoBehaviour {
     public string logText = "hello faggots";
     public float speed = 2;
+    public float health = 10;
     // Use this for initialization
     void Start () {
 
 
         Debug.Log(logText);
-        Rigidbody2D rigidbody = GetComponent<Rigidbody2D>();
-
-        GetComponent<Rigidbody2D>().velocity = Vector2.right;
+        damage(3);
+       
 
     }
 	
 	// Update is called once per frame
 	void Update ()
     {
-        
+        Rigidbody2D rigidbody = GetComponent<Rigidbody2D>();
+
+        GetComponent<Rigidbody2D>().velocity = Vector2.right;
+
+    }
+ public void damage(float damage)
+    {
+
+        health = health - damage; 
+
+
+
 
     }
 }
