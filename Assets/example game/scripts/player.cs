54634f2910fc6fe2afe0fdb768b2eba898dd9ce0
LEWIS MACGREGOR <1612679@student.fife.ac.uk>
2018-02-26 11:24:41 +0000

    example player code

--- Assets/example game/scripts/player.cs
+++ Assets/example game/scripts/player.cs
@@ -2,14 +2,23 @@
 using System.Collections;
 
 public class player : MonoBehaviour {
+    public string logText = "hello faggots";
+    public float speed = 2;
+    // Use this for initialization
+    void Start () {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
+
+        Debug.Log(logText);
+        Rigidbody2D rigidbody = GetComponent<Rigidbody2D>();
+
+        GetComponent<Rigidbody2D>().velocity = Vector2.right;
+
+    }
 	
 	// Update is called once per frame
-	void Update () {
-	
-	}
+	void Update ()
+    {
+        
+
+    }
 }
